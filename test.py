3fb0dd4da7b1c017f88e86d655b9a560e31c1b2d
Raymond Seger <raymondseger@live.com>
2016-03-15 17:41:48 +0700

    copy list

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 zs = ["hello", 2.0, 5, [10, 20]]
 
-print(zs[3][1]);
+copy_list = zs[:]
+different_value_list = zs[1:4] #get zs[1], zs[2], zs[3]
+
+#exact value
+print(copy_list)
+
+
+print(different_value_list)
