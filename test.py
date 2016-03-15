3b9562d3f3ae5c29e02f0c49793ea9c2c7a189b0
Raymond Seger <raymondseger@live.com>
2016-03-15 17:43:14 +0700

    test dictionary

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
-zs = ["hello", 2.0, 5, [10, 20]]
-
-copy_list = zs[:]
-different_value_list = zs[1:4] #get zs[1], zs[2], zs[3]
-
-#exact value
-print(copy_list)
-
-
-print(different_value_list)
+inventory = {"apples": 430, "bananas": 312, "oranges": 525, "pears": 217}
+print(inventory)
