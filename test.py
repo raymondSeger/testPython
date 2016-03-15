9580c1ba2eed2b903557c1376cc38fc562d93dc1
Raymond Seger <raymondseger@live.com>
2016-03-15 17:10:01 +0700

    test importing fibbonaci library

--- test.py
+++ test.py
@@ -1,14 +1,4 @@
-variable1 = 1;
-print('We are the {} who say "{}!"'.format('knights', 'Ni'))
+import fibonnaci
 
-try:
-    variable2 = 10 * (1/0);
-except ZeroDivisionError:
-    print("You're trying to divide by 0 dude!")
-    # raise means it will still show the error line and ends the program
-    raise
-except:
-    print("Unexpected error")
-    raise
+print(fibonnaci.fib2(1000))
 
-print("Next line")
